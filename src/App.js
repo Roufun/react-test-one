2051a7ad86cfd8d02e1cd2c721d80e1a53fc597a
Roufun <roufun@github.com>
2021-03-01 19:19:42 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Persons from './Components/Persons/Persons';
+
+function App() {
+
+  return (
+    <div className="container">
+      <h2 className='text-center text-primary py-3'> &rarr; Workplace &larr;</h2>
+      <div className="d-flex flex-wrap">
+        <Persons></Persons>
+      </div>
+    </div>
+  );
+}
+
+export default App;
